ce4bf84dc4891db94394f9cfea0aa5b883a0de59
qSlnz <hub@raffenne.fr>
2021-04-04 16:50:28 +0200

    Counter/remaining time in component

--- packages/react-app/src/components/MainScreen/Counter.tsx
+++ packages/react-app/src/components/MainScreen/Counter.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-let Counter = ({ data }: { data: number }) => {
+let Counter = ({ remTime, poolId }: { remTime: number, poolId: number }) => {
+    const [remainingTime, setRemainingTime] = useState(remTime);
+    const [timer, setTimer] = useState<NodeJS.Timeout>();
+
+    useEffect(() => {
+        console.log("CreateTimer: " + poolId);
+
+        setTimer(setInterval(() => {
+            console.log("Hehe: " + poolId);
+            setRemainingTime(oldTime => oldTime - 1 < 0 ? 0 : oldTime - 1);
+        }, 1000));
+    }, []);
+
+    useEffect(() => {
+        console.log("control: " + poolId);
+        if (timer && remainingTime <= 0) {
+            console.log("On clear: " + poolId);
+            clearInterval(timer);
+        }
+    }, [remainingTime]);
+
     return (
-        <div className="dapp-unitpool-data">
-            Remaining time: {data}s
-        </div>
+        ((remainingTime > 0 &&
+            <div className="dapp-unitpool-data">
+                🕐 Remaining time: {remainingTime}s
+            </div>) ||
+            <div className="dapp-unitpool-data">
+                ⏰ Time elapsed
+            </div>
+        )
     )
 };
 
